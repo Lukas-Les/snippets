21926a5254fdd3cee4a3b0448b57c29dee4af1be
stabmeldys <lukas.les93@gmail.com>
2024-09-03 18:21:48 +0300

    arduino chapter 2

---
+++ rust-snippets/target/debug/deps/rust_snippets-0257fb2758327a9d.d
@@ -0,0 +1,6 @@
+/mnt/e/github/snippets/rust-snippets/target/debug/deps/librust_snippets-0257fb2758327a9d.rmeta: src/lib.rs src/user_input.rs
+
+/mnt/e/github/snippets/rust-snippets/target/debug/deps/rust_snippets-0257fb2758327a9d.d: src/lib.rs src/user_input.rs
+
+src/lib.rs:
+src/user_input.rs:
