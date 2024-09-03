608ef1a9034359bc61723efe2068a6984599494c
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2024-09-03 18:26:14 +0300

    Delete rust-snippets/target directory

--- rust-snippets/target/debug/deps/rust_snippets-ba1c2c19a55c551a.d
+++
@@ -1,6 +0,0 @@
-/mnt/e/github/snippets/rust-snippets/target/debug/deps/librust_snippets-ba1c2c19a55c551a.rmeta: src/lib.rs src/user_input.rs
-
-/mnt/e/github/snippets/rust-snippets/target/debug/deps/rust_snippets-ba1c2c19a55c551a.d: src/lib.rs src/user_input.rs
-
-src/lib.rs:
-src/user_input.rs:
